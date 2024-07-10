ecc16b92a77b61326fa8733be56eed9c8aae6b30
Diego Palacio <doyola@netgate.com>
2024-07-10 17:15:50 -0500

    WOrking user api

--- config/config.go
+++ config/config.go
@@ -1,19 +1,37 @@
 package config
 
 import (
+	"codewave/models"
+	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 var DB *gorm.DB
 
 func InitDB() {
-	var err error
+	// Load the environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 	dsn := os.Getenv("DATABASE_URL")
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix: "public.",
+		},
+	})
 	if err != nil {
 		panic("Failed to connect to database!")
+	}
+	// Migrate the schema
+	modelsToMigrate := []interface{}{&models.User{}, &models.Project{}}
+	err = DB.AutoMigrate(modelsToMigrate...)
+	if err != nil {
+		panic("Failed to migrate the schema!")
 	}
 }
