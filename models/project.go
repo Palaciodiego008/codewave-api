7dbcfc36b81cf6451caeb8f87b3059e6319d79d0
Diego Palacio <doyola@netgate.com>
2024-07-19 13:03:12 -0500

    Add another form to create projects

--- models/project.go
+++ models/project.go
@@ -1,16 +1,20 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/lib/pq"
+)
 
 type Project struct {
-	ID          uint      `json:"id" gorm:"primaryKey;autoIncrement"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	Language    string    `json:"language"`
-	Backend     bool      `json:"backend"`
-	Frontend    bool      `json:"frontend"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
-	UserID      uint      `json:"user_id"`
-	User        User      `json:"user" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"` // User has many projects
+	ID          uint           `json:"id" gorm:"primaryKey;autoIncrement"`
+	Title       string         `json:"title"`
+	Description string         `json:"description"`
+	Languages   pq.StringArray `json:"languages" gorm:"type:text[]"`
+	Backend     bool           `json:"backend"`
+	Frontend    bool           `json:"frontend"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	UserID      uint           `json:"user_id"`
+	User        User           `json:"user" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"` // User has many projects
 }
